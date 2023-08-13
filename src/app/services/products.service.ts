e55bab0aa33fbc93a4ac6eb964e8ed35d2db8dd4
Dario Turin <dturin1997@gmail.com>
2023-08-13 01:07:48 -0500

    Video-02-platzi: Solicitudes GET - Se cambió la API url porque no cargaban las imagenes con la anterior

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,15 +4,19 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from './../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getAllProducts() {
-    return this.http.get<Product[]>('https://young-sands-07814.herokuapp.com/api/products');
+    /*
+    return this.http.get<Product[]>(
+      'https://young-sands-07814.herokuapp.com/api/products'
+    );
+    */
+    return this.http.get<Product[]>(
+      'https://api.escuelajs.co/api/v1/products/'
+    );
   }
 }
